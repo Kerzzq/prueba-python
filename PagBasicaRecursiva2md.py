f14ab03c7fbb7476fbf5ac7886ab53cff1726886
Salazar-Suso Iker <iker.salazar.ext@inetum.com>
2025-10-24 10:09:23 +0200

    creo que mejora

--- PagBasicaRecursiva2md.py
+++ PagBasicaRecursiva2md.py
@@ -37,6 +37,8 @@ class MarkdownExtractor:
             return
 
         soup = BeautifulSoup(resp.text, "html.parser")
+        html_fixed = soup.prettify()
+        soup = BeautifulSoup(html_fixed, "html.parser")
 
         # Usar todo el <main> (fallback: field-item)
         main_content = soup.find("main") or soup.find("div", class_="field-item")
